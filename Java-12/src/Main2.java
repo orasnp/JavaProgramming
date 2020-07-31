82bc1b9ef719c42c62350019bb3dce09d79324fe
Aleksandr (С200643) <orasenpai@gmail.com>
2020-07-31 14:36:19 +0900

    Update Main2.java

--- Java-12/src/Main2.java
+++ Java-12/src/Main2.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
+public class Main2 { 
+    public static void main(String[] args) { 
+    	double rad,area,circum;
+    	Scanner scan=new Scanner(System.in);
+    	System.out.print("Radius = ");
+    	rad = scan.nextDouble();
+    	area = Math.PI*(Math.pow(rad, 2));
+    	System.out.println("Area: " + area);
+    	circum=2*Math.PI*rad;
+    	System.out.println("Circumference:" + circum);
+    	
+    	
+    }
 
-public class Main2 {
+    } 
 
-}
+ 
